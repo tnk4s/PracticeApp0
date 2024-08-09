19cd919d9e32ee76d4662148f1e04c3b64f3afad
sataikumi <sata19300@gmail.com>
2024-08-09 20:05:08 +0900

    メインページにボタンを配置，各サブページのファイルを作成しました．

---
+++ app/src/main/java/com/example/practiceapp0/Screen1.kt
@@ -0,0 +1,28 @@
+package com.example.practiceapp0
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.practiceapp0.ui.theme.PracticeApp0Theme
+
+@Composable
+fun Screen1() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "画面1")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Screen1Preview() {
+    PracticeApp0Theme {
+        Screen1()
+    }
+}
